232e9fd68b4748afe50323b8f0b5888ef01bb85a
AngelDown12 <angeldown12xz@gmail.com>
2025-07-20 14:15:47 -0600

    Actualizar owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -2,7 +2,7 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
 let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
 
-if (!text) return m.reply(`⚡ Ingresa el enlace del Grupo.`)
+if (!text) return m.reply(`☁️ Ingresa el enlace del Grupo.`)
 let [_, code] = text.match(linkRegex) || []
 if (!code) return m.reply('Enlace invalido.')
 let res = await conn.groupAcceptInvite(code)
