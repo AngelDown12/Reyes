bc9e395321289c994cfb459c3bf6de6501f49280
AngelDown12 <angeldown12xz@gmail.com>
2025-07-20 13:41:53 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -13,13 +13,13 @@ let tags = {
   'img': '𝘐𝘮𝘢́𝘨𝘦𝘯𝘦𝘴 📸',
   'group': '𝘎𝘳𝘶𝘱𝘰𝘴 👥',
   'logo': '𝘓𝘰𝘨𝘰 - 𝘮𝘢𝘬𝘦𝘳 🎨',
-  'nable': '𝘖𝘯 / 𝘖𝘧𝘧 📴', 
+  'nable': '𝘖𝘯 / 𝘖𝘧𝘧 📴',
   'downloader': '𝘋𝘦𝘴𝘤𝘢𝘳𝘨𝘢𝘴 📥',
   'tools': '𝘏𝘦𝘳𝘳𝘢𝘮𝘪𝘦𝘯𝘵𝘢𝘴 🔧',
   'fun': '𝘋𝘪𝘷𝘦𝘳𝘴𝘪𝘰́𝘯 🎲',
-  'nsfw': '𝘕𝘴𝘧𝘸 🔞', 
-  'owner': '𝘊𝘳𝘦𝘢𝘥𝘰𝘳 😺', 
-  'audio': '𝘈𝘶𝘥𝘪𝘰𝘴 🔉', 
+  'nsfw': '𝘕𝘴𝘧𝘸 🔞',
+  'owner': '𝘊𝘳𝘦𝘢𝘥𝘰𝘳 😺',
+  'audio': '𝘈𝘶𝘥𝘪𝘰𝘴 🔉',
   'advanced': '𝘈𝘷𝘢𝘯𝘻𝘢𝘥𝘰 💠',
   'freefire': '𝘍𝘳𝘦𝘦 𝘍𝘪𝘳𝘦 📌',
   'anime': '𝘈𝘯𝘪𝘮𝘦 🌸',
@@ -28,18 +28,15 @@ let tags = {
 const defaultMenu = {
   before: `
 *꒷꒦꒷꒷꒦꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒷꒦꒷꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷*
-    
-
-
- %readmore
+%readmore
 *~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~•~*
 
 \t\t\tC H I K I S B O T 🕷️
 `.trimStart(),
-header: '┣━━━ *〔* *%category* *〕*━━━┫',
-body: '*┃⋗ 🕷️* *%cmd*\n',
-footer: '┗━━━━━━━━━━━━━━┛\n',
-after: '',
+  header: '┣━━━ *〔* *%category* *〕*━━━┫',
+  body: '*┃⋗ 🕷️* *%cmd*\n',
+  footer: '┗━━━━━━━━━━━━━━┛\n',
+  after: '',
 }
 
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
@@ -94,19 +91,22 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       if (plugin && 'tags' in plugin)
         for (let tag of plugin.tags)
           if (!(tag in tags) && tag) tags[tag] = tag
+
     conn.menu = conn.menu ? conn.menu : {}
     let before = conn.menu.before || defaultMenu.before
     let header = conn.menu.header || defaultMenu.header
     let body = conn.menu.body || defaultMenu.body
     let footer = conn.menu.footer || defaultMenu.footer
-    let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : ``) + defaultMenu.after
+    let after = conn.menu.after || defaultMenu.after
+
     let _text = [
       before,
       ...Object.keys(tags).map(tag => {
         return header.replace(/%category/g, tags[tag]) + '\n' + [
           ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
             return menu.help.map(help => {
-              return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
+              let clean = help.replace(/^menu/i, '') // ⬅️ Aquí eliminamos "menu"
+              return body.replace(/%cmd/g, menu.prefix ? clean : '%p' + clean)
                 .replace(/%islimit/g, menu.limit ? '' : '')
                 .replace(/%isPremium/g, menu.premium ? '' : '')
                 .trim()
@@ -117,7 +117,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       }),
       after
     ].join('\n')
-    let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
+
     let replace = {
       '%': '%',
       p: _p, uptime, muptime,
@@ -128,7 +128,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       version: _package.version,
       npmdesc: _package.description,
       npmmain: _package.main,
-      author: _package.author.name,
+      author: _package.author?.name || '',
       license: _package.license,
       exp: exp - min,
       maxexp: xp,
@@ -138,14 +138,13 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       level, limit, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg,
       readmore: readMore
     }
-    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
 
-    let pp = 'https://files.catbox.moe/cqyt08.mp4'
+    let text = _text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     await conn.sendMessage(m.chat, {
-  video: { url: 'https://files.catbox.moe/cqyt08.mp4' },
-  caption: text.trim(),
-  gifPlayback: true
-}, { quoted: m })
+      video: { url: 'https://files.catbox.moe/cqyt08.mp4' },
+      caption: text.trim(),
+      gifPlayback: true
+    }, { quoted: m })
 
   } catch (e) {
     conn.reply(m.chat, 'Lo sentimos, el menú tiene un error.', m)
@@ -153,11 +152,11 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   }
 }
 
-handler.customPrefix = /^(menu|menú|ayuda|help)$/i;
-handler.command = new RegExp; // para que funcione sin prefijo
-handler.register = false;
+handler.customPrefix = /^(menu|menú|ayuda|help)$/i
+handler.command = new RegExp // sin prefijo
+handler.register = false
 
-export default handler;
+export default handler
 
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
@@ -167,4 +166,4 @@ function clockString(ms) {
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
-        }
+}
