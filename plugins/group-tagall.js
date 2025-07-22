f44d07e0eb8fdb00bfce27c7a6db0f3d1937ec79
AngelDown12 <angeldown12xz@gmail.com>
2025-07-21 22:49:46 -0600

    Actualizar group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -9,7 +9,7 @@ const handler = async (m, { conn, participants, isAdmin, isOwner }) => {
     .replace(/^(\.|\/)?(tagall|invocar|invocacion|invocación|todos|talibanes)/i, '')
     .trim();
 
-  const emojis = ['🕷️', '🕷️'];
+  const emojis = ['🤴', ''];
   const lista = participants
     .map((u, i) => `${emojis[i % emojis.length]} @${u.id.split('@')[0]}`)
     .join('\n');
